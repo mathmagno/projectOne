3d2b9874e9e9a6e2236e9de1df8f0bae402a41e3
math_magno <98839706+mathmagno@users.noreply.github.com>
2025-12-30 20:07:38 -0300

    add

---
+++ API/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/users', (req, res) => {
+    res.send('Ok, deu bom')
+});
+
+app.listen(3000);
+
+
+
+
+
+/* 
+1 Tipo de rota / Método HTTP
+2 Endereço 
+
+*/
