e23d2c21d6fc52f074422ad37091c8d1cf90c993
math_magno <98839706+mathmagno@users.noreply.github.com>
2026-01-02 20:11:57 -0300

    add

--- API/server.js
+++ API/server.js
@@ -1,13 +1,23 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/users', (req, res) => {
+    
+    users.push(req.body)
+    
+    res.send('Ok post')
+})
+
 
 app.get('/users', (req, res) => {
-    res.send('Ok, deu bom')
-});
+    res.json(users)
+})
 
 app.listen(3000)
-
 
 
 
