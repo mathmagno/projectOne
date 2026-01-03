f4ec3820637fdffc41d17af57ce0e5239e6432a1
math_magno <98839706+mathmagno@users.noreply.github.com>
2026-01-03 11:33:47 -0300

    add

--- API/server.js
+++ API/server.js
@@ -1,19 +1,30 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
+// VARIÁVEL DO TIPO ARRAY PARA GUARDAR USUÁRIOS
 const users = []
 
-app.post('/users', (req, res) => {
+// ENVIAR USUÁRIO PARA O CONJUNTO DE DADOS users = []
+app.post('/users', async (req, res) => {
     
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name:  req.body.name,
+            age:   req.body.age
+        }
+    })
 
     res.status(201).json(req.body)
 })
 
 
-
+// CONSULTAR USUÁRIOS EXISTENTES EM users = []
 app.get('/users', (req, res) => {
     
     res.status(200).json(users)
